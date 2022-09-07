4c74d115fa92e739b2d6d4ed231dbd82254fdca1
Dario <kayle958@gmail.com>
2022-09-07 20:50:22 +0200

    Adding Swagger

--- SpotifyWebApi/Program.cs
+++ SpotifyWebApi/Program.cs
@@ -12,6 +12,7 @@ builder.Services.AddDbContext<TokenDBContext>(options => options.UseSqlServer(bu
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 
 
@@ -22,6 +23,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
