84dd714eda381bfbdce1dac4feac52fd5369aed4
Dario <kayle958@gmail.com>
2022-09-09 05:40:37 +0200

    Add

--- SpotifyWebApi/Program.cs
+++ SpotifyWebApi/Program.cs
@@ -14,12 +14,15 @@ internal class Program
         builder.Services.AddControllers();
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
 
         var app = builder.Build();
 
         // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
         {
+            app.UseSwagger();
+            app.UseSwaggerUI();
         }
 
         app.UseHttpsRedirection();
