a16838776ea47f1fe6b67af45ccadf4c9ae0d936
Dario <kayle958@gmail.com>
2022-09-14 11:41:22 +0200

    Index

---
+++ SpotifyWebApi/Controllers/Index.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SpotifyWebApi.Controllers
+{
+    [Route("index")]
+    [ApiController]
+    public class Index : ControllerBase
+    {
+
+        [HttpGet]
+        public string Indext()
+        {
+            return "Hello,Welcome";
+        }
+    }
+}
