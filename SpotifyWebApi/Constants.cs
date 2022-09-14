60d73829bdde15a555a1dbb3197bc87caa5a26e9
Dario <kayle958@gmail.com>
2022-09-14 12:12:01 +0200

    Change

--- SpotifyWebApi/Constants.cs
+++ SpotifyWebApi/Constants.cs
@@ -3,7 +3,7 @@
     public class Constants
     {
         public const string ClientId = "111e7d05fa434a22b23477d52f654f6f";
-        public const string redirectUri = "https://spotifywebapi-production.up.railway.app/api/Spotify/Auth/callback";
+        public const string redirectUri = "https://spotifywebapi20220909031427.azurewebsites.net/api/Spotify/Auth";
         public const string clientSecret = "41715f2f289c4c929c772745cf9be786";
         public const string grant_type = "authorization_code";
 
