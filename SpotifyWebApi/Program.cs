5a06140e14cfd3ab39713c833a117464daac1905
Dario <kayle958@gmail.com>
2022-09-14 10:30:12 +0200

    Change in program

--- SpotifyWebApi/Program.cs
+++ SpotifyWebApi/Program.cs
@@ -8,6 +8,29 @@ namespace SpotifyWebApi
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllers();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapRazorPages();
+
+            app.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args)
