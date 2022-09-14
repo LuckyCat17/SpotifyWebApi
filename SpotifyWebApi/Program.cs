72ea89a20574a79fbd35462e2432a527057ca475
Dario <kayle958@gmail.com>
2022-09-14 11:04:12 +0200

    Use Program.cs

--- SpotifyWebApi/Program.cs
+++ SpotifyWebApi/Program.cs
@@ -2,17 +2,17 @@
 global using Microsoft.EntityFrameworkCore;
 using SpotifyWebApi;
 using SpotifyWebApi.Controllers;
+using Microsoft.AspNetCore.Hosting;
 
 namespace SpotifyWebApi
 {
     public class Program
     {
         public static void Main(string[] args) {
+            CreateWebHostBuilder(args).Build().Run();
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
-
             builder.Services.AddDbContext<TokenDBContext>(options => options.UseSqlServer("Data Source = 104.198.162.41; Initial Catalog = SpotifyWebApi; User ID = provola12345; Password =vL88/6X@Z*9PMGT<"));
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -36,7 +36,15 @@ namespace SpotifyWebApi
 
             app.Run();
         }
+       public static IHostBuilder CreateWebHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Program>();
+                });
     }
+
+ 
 }
     
         
